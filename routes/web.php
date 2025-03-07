1b6e9179d2f88e4b0ec6a518861703108e71bf8f
Wahib <nurwahibzakkiy@gmail.com>
2025-03-07 22:30:04 +0700

    Praktikum 2 - Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,33 +19,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'about']);
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
 
-Route::get('/hello', function () {
-    return 'Hello World';
-});
+
 Route::get('/world', function () {
     return 'World';
-}); 
+});
 Route::get('/', function () {
     return 'Selamat Datang';
 });
-Route::get('/about', function () {
-    return '2341720154  Nurwahib Zakki Yahya';
-});
+
 Route::get('/user/{name}', function ($name) {
-    return 'nama saya: '.$name;
+    return 'nama saya: ' . $name;
 });
-Route::get('/posts/{posts}/comments/{comments}', function ($postId,$commentId) {
-    return 'Pos ke-'. $postId. " Komentar ke-: ". $commentId;
+Route::get('/posts/{posts}/comments/{comments}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
 });
-Route::get('/articles/{id}', function ($articlesId,) {
-    return 'Halaman Artikel ke-'. $articlesId;
-});
-
-Route::get('/user/{name?}', function ($name= 'John') {
-    return 'nama saya: '.$name;
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'nama saya: ' . $name;
 });
 
+Route::resource('photos',PhotoController::class)->only([
+    'index',
+    'show'
+]);
+
+Route::resource('photos',PhotoController::class)->except([
+    'create',
+    'store',
+    'update',
+    'destroy',
+]);
