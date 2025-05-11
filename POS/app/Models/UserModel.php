e62be50b9f5b1031de903eeb40e281d94812efde
Wahib <nurwahibzakkiy@gmail.com>
2025-05-11 14:41:13 +0700

    Jobsheet 7 - Praktikum 1 Implementasi Authentication

--- POS/app/Models/UserModel.php
+++ POS/app/Models/UserModel.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
 
 class UserModel extends Model
 {
@@ -13,10 +14,14 @@ class UserModel extends Model
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
 
-    protected $fillable = ['level_id','username', 'nama','password'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
 
-    public function level(): BelongsTo{
-        return $this->belongsTo(LevelModel::class, 'level_id','level_id');
+    protected $hidden = ['password']; // jangan ditampilkan pada saat di select
+
+    protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
 
 }
