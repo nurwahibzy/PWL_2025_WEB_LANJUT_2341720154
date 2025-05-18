8963538d2963db86c4c8d6dd059711c628d12a26
Wahib <nurwahibzakkiy@gmail.com>
2025-05-19 00:13:33 +0700

    Jobsheet 8 - Praktikum 1 – Implementasi Upload File untuk import data

--- POS/routes/web.php
+++ POS/routes/web.php
@@ -105,6 +105,8 @@ Route::middleware(['auth'])->group(function () {
             Route::put('/{id}/update_ajax', [BarangController::class, 'update_ajax']);
             Route::get('/{id}/delete_ajax', [BarangController::class, 'confirm_ajax']);
             Route::delete('/{id}/delete_ajax', [BarangController::class, 'delete_ajax']);
+            Route::get('/import', [BarangController::class, 'import']);
+            Route::post('/import_ajax', [BarangController::class, 'import_ajax']);
         }
     );
 });
